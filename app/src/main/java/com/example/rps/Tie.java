4c3ce6733ae301150f98f1c4a383499263d7b2e2
Meera_User1 <meeraj2@illinois.edu>
2019-12-12 13:17:02 -0600

    final commit. app is ready to use

--- app/src/main/java/com/example/rps/Tie.java
+++ app/src/main/java/com/example/rps/Tie.java
@@ -16,7 +16,7 @@ public class Tie extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tie);
 
-        int resID = getResources().getIdentifier("tieMusic", "raw", getPackageName());
+        int resID = getResources().getIdentifier("tiemusic", "raw", getPackageName());
         tieMusic = MediaPlayer.create(this, resID);
         tieMusic.start();
 
